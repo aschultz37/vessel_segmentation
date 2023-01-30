b8f81fb88a8e8deea14d095e41cbe2c23a815a5e
aschultz37 <austin.schultz@nyulangone.org>
2023-01-30 09:36:08 -0500

    initial commit

---
+++ preprocess_files.py
@@ -0,0 +1,8 @@
+'''Filters vessels based on area.
+   Then calculates vessel type ID based on
+   marker thresholds. Input and output are .csv.'''
+
+import os
+import numpy as np
+import pandas as pd
+
